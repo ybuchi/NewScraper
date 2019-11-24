ac4449a153f2001b351eeae5d7df386957d1f651
ybuchi <yabuch_ch@hotmail.com>
2019-11-24 17:24:50 -0500

    Added the view folder and the handlebars files

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 //Require our dependencies
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
 
 //Set up the port to be either host's deisgnated port or port 3000
 var PORT = process.env.PORT || 3000;
@@ -11,6 +14,19 @@ var app = express();
 
 //!! Designate Public folder as static directory!!//
 app.use(express.static(__dirname + "public"));
+
+//Connect to express handlebars
+app.engine("handlebars", expressHandlebars({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//Set up body parser
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+//Every request will go through to the router
 app.use(router);
 
 //Listen to the port
